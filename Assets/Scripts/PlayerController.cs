995bc2dd86a81731f842bf6dc168dea5447e99db
cayo09 <chermit0928@gmail.com>
2025-04-06 19:08:09 -0500

    Movimiento y camara arreglados

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float runSpeed;
     [SerializeField] private float speedRotation;
     [SerializeField] private float jumpForce;
-    private bool canJump = false;
 
+    private bool canJump = false;
+    float currentSpeed = 0;
+    bool isRunning = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -28,21 +31,43 @@ public class PlayerController : MonoBehaviour
     {
         float verticalInput = Input.GetAxis("Vertical"); // W/S
         float horizontalInput = Input.GetAxis("Horizontal"); // A/D
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) && verticalInput > 0;
 
-        // Definir velocidad según si está corriendo o caminando
-        float currentSpeed = isRunning ? runSpeed : walkSpeed;
-        currentSpeed *= verticalInput; // Si presiona "S", la velocidad será negativa
+        //logica para aplicar del movimiento
+        if (verticalInput != 0 || horizontalInput != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isRunning = true;
+                currentSpeed = runSpeed;
+            }
+            else
+            {
+                isRunning = false;
+                currentSpeed = walkSpeed;
+            }
+        }
+        else
+        {
+            currentSpeed = 0;
+        }
+
+        //Moverse
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * walkSpeed * Time.deltaTime);
+
+        //Rotar la camara con el Mouse
+        float rotcamX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, rotcamX * speedRotation * Time.deltaTime, 0);
 
         // Aplicar movimiento
-        Vector3 movement = transform.forward * currentSpeed * Time.deltaTime;
-        rb.MovePosition(transform.position + movement);
+        //Vector3 movement = transform.forward * currentSpeed * Time.deltaTime;
+        //rb.MovePosition(transform.position + movement);
+
 
         // Rotación si el personaje se mueve
-        if (movement.magnitude > 0)
+        /*if (movement.magnitude > 0)
         {
             transform.Rotate(0, horizontalInput * speedRotation * Time.deltaTime, 0);
-        }
+        }*/
 
         // Pasar valores al Animator
         animator.SetFloat("Speed", Mathf.Abs(currentSpeed));
